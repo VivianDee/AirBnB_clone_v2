92c8a4baf6adc20c0d54982a8868e92e1b1bdb3d
Dagbue Vivian <vdagbue@gmail.com>
2023-09-19 15:30:12 +0000

    clean up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
 
 GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

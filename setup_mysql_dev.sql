1352485074317604f005426c2b030c8b341d8751
Dagbue Vivian <vdagbue@gmail.com>
2023-09-19 15:23:58 +0000

    Changed database password

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
 -- Prepares a MySQL server for teats
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY '123456';
-
 GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

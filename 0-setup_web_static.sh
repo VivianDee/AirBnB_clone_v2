fe66410ae67ea133d0e45a1ca1564b156d611154
VivianDee <vdagbue@gmail.com>
2023-10-05 14:29:51 +0000

    Fix: Ownerhip

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,6 @@ echo "<html>
   </body>
 </html>" | sudo tee "$path/index.html"
 sudo ln -sf "$path" /data/web_static/current
-my_username=$(whoami)
-sudo chown -R "$my_username":"$my_username" /data
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "57i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart

264479334182d4205fcfc444d6e6e32a27c9ae7f
VivianDee <vdagbue@gmail.com>
2023-10-05 14:19:37 +0000

    Synthax erreo

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This script sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+path="/data/web_static/releases/test"
+sudo mkdir -p "$path"
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee "$path/index.html"
+sudo ln -sf "$path" /data/web_static/current
+my_username=$(whoami)
+sudo chown -R "$my_username":"$my_username" /data
+sudo sed -i "57i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart

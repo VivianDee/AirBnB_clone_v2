f79da9aaac819bd19f7377b4a186f81e56fc1a99
VivianDee <vdagbue@gmail.com>
2023-10-05 20:15:25 +0000

    Pycodestyle error

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,6 +5,7 @@
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
+
 def deploy():
     """Creates and distributes an archive to my web servers"""
     archive_path = do_pack()

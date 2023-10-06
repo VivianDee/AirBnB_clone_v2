806b30584febece5bf164de18b68a466645967ee
VivianDee <vdagbue@gmail.com>
2023-10-06 09:28:18 -0700

    Fix: Algorithm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 # This script sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+
 path="/data/web_static/releases/test"
+path2="/data/web_static/shared"
 sudo mkdir -p "$path"
+sudo mkdir -p "$path2"
+
 echo "<html>
   <head>
   </head>
@@ -11,8 +17,15 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee "$path/index.html"
+
+symlink="/data/web_static/current"
+if [ -L "$symlink" ]; then
+    sudo rm "$symlink"
+fi
 sudo ln -sf "$path" /data/web_static/current
+
 my_username=$(whoami)
 sudo chown -R "$my_username":"$my_username" /data/
 sudo sed -i "57i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+
 sudo service nginx restart

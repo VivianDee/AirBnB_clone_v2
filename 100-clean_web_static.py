ddf52922b1328c187cfc380d6512d45bb99ff4fd
VivianDee <vdagbue@gmail.com>
2023-10-07 08:54:22 +0000

    Advanced task

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Fabric script that deletes out-of-date archives."""
+from fabric.api import *
+import os
+
+
+env.hosts = ['100.24.205.80', '54.85.130.183']
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives."""
+    number = int(number)
+    if number < 2:
+        number = 1
+    else:
+        number += 1
+
+    archives_path = "/data/web_static/releases"
+    with lcd("versions"):
+        local("ls -t | tail -n +{} | xargs -I {{}} rm {{}}".format(number))
+    with cd(archives_path):
+        run("sudo ls -t | sudo tail -n +{} | sudo xargs -I {{}} sudo rm -rf {{}}".format(number))
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,20 +3,12 @@
     This script creates and distributes an archive to my web servers.
 """
 do_pack = __import__('1-pack_web_static').do_pack
-from fabric.api import env
-from fabric.api import local
-from fabric import *
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 def deploy():
     """Creates and distributes an archive to my web servers"""
     archive_path = do_pack()
-
     if not archive_path:
         return False
-
-    result = local(
-        "fab -f 2-do_deploy_web_static.py do_deploy:archive_path={} -i {} -u {}".format(
-            archive_path, env.key_filename[0], env.user), capture=True)
-    print(result.stdout)
-    return result.succeeded
+    return do_deploy(archive_path)

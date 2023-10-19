54c2bdad421fb95b837bda89213ade6c5c5f5831
Dagbue Vivian <vdagbue@gmail.com>
2023-10-19 15:16:37 +0000

    Web framework"

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Display the homepage"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Display the homepage"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text=None):
+    """Displays text from the URL"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text="is_cool"):
+    """Displays text from the URL"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(escape(text))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n=0):
+    """Displays number from the URL"""
+    return '{} is a number'.format(escape(0))
+
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=5000, debug=True)

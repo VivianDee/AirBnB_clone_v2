5bb6fecd371ae13481a89439a52a05e76cfb1acd
VivinDee <vdagbue@gmail.com>
2023-10-25 14:23:24 +0000

    Add: Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,22 +3,33 @@
 from flask import Flask, render_template
 from markupsafe import escape
 from models.__init__ import storage
+from models.state import State
 
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays content from my database"""
-    all_objs = storage.all(State).values()
-
-    return render_template("7-states_list.html", all_objs=all_objs)
 
 
 @app.teardown_appcontext
 def reload_db(error=None):
     """Closes the current database session"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays content from my database"""
+    all_objs = storage.all(State).values()
+    all_objs = sorted(all_objs, key=lambda state: state.name)
+
+    return render_template("7-states_list.html", all_objs=all_objs)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays content from my database"""
+    all_objs = storage.all(State).values()
+    all_objs = sorted(all_objs, key=lambda state: state.name)
+
+    return render_template("8-cities_by_states.html", all_objs=all_objs)
 
 
 if __name__ == "__main__":
